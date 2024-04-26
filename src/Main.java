19a878c4fedc3e73ee9354a2ca8efb5b52b4a65a
Luca Just <ljust3006@gmail.com>
2024-04-25 20:04:34 -0400

    Adicao aluno 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell owrld");
+        new Tela1().setVisible(true);
     }
 }
---
+++ src/Tela1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Tela1 extends JFrame {
+    private JTextField nome;
+    private JTextField validade;
+
+    public Tela1(){
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        setTitle("Atividade");
+        nome = new JTextField(20);
+        validade = new JTextField(20);
+        add(nome);
+        add(validade);
+        pack();
+    }
+}
